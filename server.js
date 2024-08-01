8ef6d68bdb29ee01782cc8ca7a674f0a0c273d2b
Azaz <azaz.ali1706@gmail.com>
2024-08-01 15:19:04 +0530

    postgre with prima

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const app= express()
+const PORT= process.env.PORT || 9000
+import "dotenv/config"
+
+app.route("/", (req, res)=>{
+    return res.json({message:"working"})
+})
+
+app.listen(PORT, ()=> console.log(`Server is running on PORT ${PORT}`))
